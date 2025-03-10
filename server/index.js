d6639b0ac9ee81708475ef2b9036933b83d7f00d
vignesh170503 <vigneshalogroups@gmail.com>
2025-03-10 11:50:24 +0530

    update changes

--- server/index.js
+++ server/index.js
@@ -17,7 +17,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001,https://merntask-taskmanager.netlify.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
