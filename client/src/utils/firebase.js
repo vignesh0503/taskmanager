4847a16bf4e1ae47e90180979fb8007f11be59cb
vignesh170503 <vigneshalogroups@gmail.com>
2025-03-11 17:26:23 +0530

    firebase changes

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -8,14 +8,12 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-
-  authDomain: "taskmanagertask.firebaseapp.com",
-  projectId: "taskmanagertask",
-  // storageBucket: "taskmanagertask.firebasestorage.app",
-  storageBucket: "taskmanagertask.appspot.com",
-  messagingSenderId: "567666481486",
-  appId: "1:567666481486:web:69afaecb959d612bc4e61f",
-  measurementId: "G-0RMST6T79Y",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
