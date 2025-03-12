2b08ac3c7e1c5d0df3dd52de15ddf00bfcc28220
vignesh170503 <vigneshalogroups@gmail.com>
2025-03-12 12:19:24 +0530

    cors chnages

--- server/index.js
+++ server/index.js
@@ -15,16 +15,16 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:3000", // Allow local development
-  "https://merntask-task-manager.netlify.app", // Allow production frontend
-];
-
 app.use(
   cors({
-    origin: allowedOrigins,
-    credentials: true, // Allow cookies & auth headers
+    origin: [
+      "https://merntask-task-manager.netlify.app",
+      "http://localhost:3000",
+    ],
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Set-Cookie"],
   })
 );
 
