375ea63cb33aad173143f9303f19d71a231245b0
vignesh170503 <vigneshalogroups@gmail.com>
2025-03-12 12:54:22 +0530

    sidebar css changes

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -61,45 +61,49 @@ const Sidebar = () => {
     dispatch(setOpenSidebar(false));
   };
 
-  const NavLink = ({ el }) => {
-    return (
-      <Link
-        to={el.link}
-        onClick={closeSidebar}
-        className={clsx(
-          "w-full lg:w-3/4 flex gap-2 px-3 py-2 rounded-full items-center text-gray-800 text-base hover:bg-[#2564ed2d] ",
-          path === el.link.split("/")[0] ? "bg-blue-700 text-neutral-100" : ""
-        )}
-      >
-        {el.icon}
-        <span className="hover:text=[#2564ed]"> {el.label}</span>
-      </Link>
-    );
-  };
-
+const NavLink = ({ el }) => {
   return (
-    <div className="w-full h-full flex flex-col gap-6 p-5">
-      <h1 className="flex gap-1 items-center">
-        <p className="bg-blue-600 p-2 rounded-full">
-          <MdOutlineAddTask className="text-white text-2xl font-black" />
-        </p>
-        <span className="text-2xl font-bold text-black">Task Me</span>
-      </h1>
-
-      <div className="flex-1 flex flex-col gap-y-5 py-8">
-        {sidebarLinks.map((link) => (
-          <NavLink el={link} key={link.label} />
-        ))}
-      </div>
-
-      <div className="">
-        <button className="w-full flex gap-2 p-2 items-center text-lg text-gray-800">
-          <MdSettings />
-          <span>Settings</span>
-        </button>
-      </div>
-    </div>
+    <Link
+      to={el.link}
+      onClick={closeSidebar}
+      className={clsx(
+        "w-full lg:w-full flex gap-3 p-3 rounded-full items-center text-gray-800 text-base transition-all duration-300",
+        "hover:bg-[#2564ed2d] hover:text-black-100 ",
+        path === el.link.split("/")[0]
+          ? "bg-blue-700 text-neutral-100 hover:bg-blue-800"
+          : ""
+      )}
+    >
+      {el.icon}
+      <span> {el.label}</span>
+    </Link>
   );
+};
+
+return (
+  <div className="w-full min-h-screen flex flex-col gap-6 p-5 overflow-y-auto">
+    <h1 className="flex gap-2 items-center">
+      <p className="bg-blue-600 p-2 rounded-full">
+        <MdOutlineAddTask className="text-white text-2xl font-black" />
+      </p>
+      <span className="text-2xl font-bold text-black">Task Me</span>
+    </h1>
+
+    <div className="flex-1 flex flex-col gap-y-5 py-8">
+      {sidebarLinks.map((link) => (
+        <NavLink el={link} key={link.label} />
+      ))}
+    </div>
+
+    <div>
+      <button className="w-full flex gap-2 p-3 items-center text-lg text-gray-800 transition-all duration-300 hover:bg-[#2564ed2d] hover:text-[#2564ed] rounded-full">
+        <MdSettings />
+        <span>Settings</span>
+      </button>
+    </div>
+  </div>
+);
+
 };
 
 export default Sidebar;
